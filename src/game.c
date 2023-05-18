44c2aa51b2dffc268f15dc74e978e2883e1729b9
Zani Felipe <62755729+FelipeZani@users.noreply.github.com>
2023-05-18 14:59:41 +0200

    Add files via upload

--- src/game.c
+++ src/game.c
@@ -6,7 +6,8 @@ Game * gameSetup()
     new_game->player = playerSetup();
     new_game->map = createMap(MAP_HEIGHT, MAP_WIDTH);	
     SetRandomSpawn(new_game);
-	AddMob(new_game->map->tiles);				
+	AddMob(new_game->map->tiles);
+					
     return new_game;
 }
 
@@ -15,7 +16,7 @@ void gameLoop(WINDOW * main_window)
     Game * game = gameSetup();
     Position position_offset;
     int input = 0;
-    
+    int count=0;
     while (input != 'a')
     {
         position_offset = handleInput(input);
